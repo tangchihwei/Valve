25fcfc91eee3b7bd0134d0bc05956546b14ca54a
Chih-Wei Tang <4719940+tangchihwei@users.noreply.github.com>
2018-07-17 21:07:22 -0700

    initial

---
+++ valve.ino
@@ -0,0 +1,61 @@
+
+int ain1 = 7;
+int ain2 = 5;
+int stby = 6;
+int pwm = 4;
+
+void turnOff() {
+  digitalWrite(pwm, LOW);
+  digitalWrite(ain1, LOW);
+  digitalWrite(ain2, HIGH); //CCW
+  digitalWrite(pwm, HIGH);
+  delay(30);
+  digitalWrite(pwm, LOW);
+}
+
+void turnOn () {
+  digitalWrite(pwm, LOW);
+  digitalWrite(ain2, LOW);
+  digitalWrite(ain1, HIGH); //CW
+  digitalWrite(pwm, HIGH);
+  delay(30);
+  digitalWrite(pwm, LOW);
+  
+}
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(ain1, OUTPUT);
+  pinMode(ain2, OUTPUT);
+  pinMode(stby, OUTPUT);
+  pinMode(pwm, OUTPUT);
+  digitalWrite(ain1, LOW);
+  digitalWrite(ain2, LOW);
+  digitalWrite(stby, HIGH);
+  digitalWrite(pwm, LOW);
+  Serial.begin(9600);
+
+}
+
+void loop() {
+  if (Serial.available()){
+    if (Serial.read() == '1'){
+      turnOn();
+      Serial.println("On");
+    }
+    else{
+      turnOff();
+      Serial.println("Off");
+    }
+  }
+  delay(1000);
+//  delay(1000);
+//  turnCW();
+//  Serial.println("CW");
+//  delay(3000);
+//  turnCCW();
+//  Serial.println("CCW"); 
+//  delay(3000);
+  
+
+}
