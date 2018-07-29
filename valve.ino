e36b707776dff0552dd9b15d1e7dc3f2e82511fd
Chih-Wei Tang <4719940+tangchihwei@users.noreply.github.com>
2018-07-28 23:28:17 -0700

    gen2 working modularized

--- valve.ino
+++ valve.ino
@@ -6,7 +6,35 @@ range readings are in units of mm.
 #include <Wire.h>
 #include <VL53L1X.h>
 
-VL53L1X sensor;
+VL53L1X gen2;
+
+void gen2_setup(){
+ gen2.setTimeout(500);
+  if (!gen2.init())
+  {
+    Serial.println("Failed to detect and initialize gen2 sensor!");
+    while (1);
+  }
+  
+
+  // Use long distance mode and allow up to 50000 us (50 ms) for a measurement.
+  // You can change these settings to adjust the performance of the sensor, but
+  // the minimum timing budget is 20 ms for short distance mode and 33 ms for
+  // medium and long distance modes. See the VL53L1X datasheet for more
+  // information on range and timing limits.
+  gen2.setDistanceMode(VL53L1X::Long);
+  gen2.setMeasurementTimingBudget(50000);
+
+  Serial.print("old address: ");
+  Serial.println(gen2.getAddress());
+  gen2.setAddress(0x30);
+  Serial.print("new address: ");
+  Serial.println(gen2.getAddress());
+
+  gen2.startContinuous(50);
+}
+
+
 
 void setup()
 {
@@ -14,31 +42,17 @@ void setup()
   Wire.begin();
   Wire.setClock(400000); // use 400 kHz I2C
 
-  sensor.setTimeout(500);
-  if (!sensor.init())
-  {
-    Serial.println("Failed to detect and initialize sensor!");
-    while (1);
-  }
-  
-  // Use long distance mode and allow up to 50000 us (50 ms) for a measurement.
-  // You can change these settings to adjust the performance of the sensor, but
-  // the minimum timing budget is 20 ms for short distance mode and 33 ms for
-  // medium and long distance modes. See the VL53L1X datasheet for more
-  // information on range and timing limits.
-  sensor.setDistanceMode(VL53L1X::Long);
-  sensor.setMeasurementTimingBudget(50000);
+  gen2_setup();
 
   // Start continuous readings at a rate of one measurement every 50 ms (the
   // inter-measurement period). This period should be at least as long as the
   // timing budget.
-  sensor.startContinuous(50);
 }
 
 void loop()
 {
-  Serial.print(sensor.read()/25.4);
-  if (sensor.timeoutOccurred()) { Serial.print(" TIMEOUT"); }
+  Serial.print(gen2.read());
+  if (gen2.timeoutOccurred()) { Serial.print("Gen2 TIMEOUT"); }
 
   Serial.println();
 }
